b85e0dde79954a11f0a5b269d58e052120109c38
moschidisg <mosxidismosxidis@gmail.com>
2022-09-25 17:59:24 +0300

    added menu items

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,43 @@
 export function menuPageLoad() {
+    const menuItems = {
+        item1: {
+            name: "BAO BUN WITH PEKING DUCK",
+            image: "../src/assets/menu-images/food1.jpg"
+        },
+        item2: {
+            name: "POKE BOWL",
+            image: "../src/assets/menu-images/food2.jpg"
+        },
+        item3: {
+            name: "INSIDE OUT COMBO",
+            image: "../src/assets/menu-images/food3.jpg"
+        },
+        item4: {
+            name: "SALMON TERYAKI",
+            image: "../src/assets/menu-images/food4.jpg"
+        },
+        item5: {
+            name: "SALAD WITH CHICKEN & AVOCADO",
+            image: "../src/assets/menu-images/food5.jpg"
+        },
+        item6: {
+            name: "MIXED SEAFOOD APPETIZERS",
+            image: "../src/assets/menu-images/food6.jpg"
+        },
+        item7: {
+            name: "BUDDHA BOWL",
+            image: "../src/assets/menu-images/food7.jpg"
+        },
+        item8: {
+            name: "CHICKEN WINGS",
+            image: "../src/assets/menu-images/food8.jpg"
+        },
+
+
+    }
+    
     const content = document.querySelector("#content");
     content.innerHTML = "";
-    console.log("menu loaded");
+    
+
 }
