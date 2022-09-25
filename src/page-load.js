2da38756e07efea35297ae327c870050234eea52
moschidisg <mosxidismosxidis@gmail.com>
2022-09-25 19:43:56 +0300

    created footer

--- src/page-load.js
+++ src/page-load.js
@@ -3,6 +3,7 @@ function initialPageLoad() {
 
     container.appendChild(createHeader());
     container.appendChild(createContent());
+	container.appendChild(createFooter());
 }
 
 function createHeader() {
@@ -61,4 +62,57 @@ function createContent() {
     return content;
 }
 
-export {initialPageLoad, createHeader};
+function createFooter() {
+	const footer = document.createElement("div");
+	footer.id = "footer";
+
+	const footerText = document.createElement("p");
+	footerText.textContent = "© Copyright 2022 Asian House";
+
+	footer.appendChild(footerText);
+
+	const footerfindus = document.createElement("p");
+	footerfindus.textContent = "Find us on:";
+
+	footer.appendChild(footerfindus);
+
+	const socialmedia = document.createElement("ul");
+	socialmedia.className = "social";
+
+	const facebook = document.createElement("li");
+	const instagram = document.createElement("li");
+	const tripadvisor = document.createElement("li");
+
+	/* const facebookImg = document.createElement("img");
+	facebookImg.src = "../src/assets/icons/facebook.png"
+	const instagramImg = document.createElement("img");
+	instagramImg.src = "../src/assets/icons/instagram.png"
+	const tripadvisorImg = document.createElement("img");
+	tripadvisorImg.src = "../src/assets/icons/tripadvisor.png"; */
+
+	const facebookLink = document.createElement("a");
+	const instagramLink = document.createElement("a");
+	const tripadvisorLink = document.createElement("a");
+
+	facebookLink.className = "fa fa-facebook";
+	instagramLink.className = "fa fa-instagram";
+	tripadvisorLink.className = "fa fa-tripadvisor";
+
+	facebookLink.href = "https://www.facebook.com/AsianHouseGR/";
+	instagramLink.href = "https://www.instagram.com/asianhousegr/"
+	tripadvisorLink.href = "https://www.tripadvisor.co.uk/Restaurant_Review-g189473-d13009795-Reviews-Asian_House-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html"
+
+	facebook.appendChild(facebookLink);
+	instagram.appendChild(instagramLink);
+	tripadvisor.appendChild(tripadvisorLink);
+	
+	socialmedia.appendChild(facebook);
+	socialmedia.appendChild(instagram);
+	socialmedia.appendChild(tripadvisor);	
+
+	footer.appendChild(socialmedia);
+
+	return footer
+}
+
+export {initialPageLoad, createHeader, createFooter};
